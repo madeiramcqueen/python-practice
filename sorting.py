cb334167981ad25a1986c524ca37d1756241fe78
madeiramcqueen <madeira@mcqueen.net>
2023-01-24 20:06:52 -0800

    Sorting, reverse, and length practice

---
+++ sorting.py
@@ -0,0 +1,18 @@
+print("Python sorting practice!")
+
+countries = ["Vietnmam", "Thailand", "Cambodia", "Peru", "Alaska"]
+print(countries)
+print("My list using sorted method:")
+print(sorted(countries))
+print("My list as original:")
+print(countries)
+print("My list in alphabetical reverse order using sort = true method:")
+countries.sort(reverse=True)
+print(countries)
+print("My list as original:")
+print(countries)
+print("Countries in reverse order using reverse method:")
+countries.reverse()
+print(countries)
+print("Length of countries:")
+print(len(countries))
