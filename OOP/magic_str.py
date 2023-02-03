4094ecc72ee609db1ed7f9e1396249b45ec6be46
madeiramcqueen <madeira@mcqueen.net>
2023-02-03 12:52:51 -0800

    add ge and lt methods to compare greater than and less than relationships

--- OOP/magic_str.py
+++ OOP/magic_str.py
@@ -23,17 +23,35 @@ class Book:
 
         return (self.title == value.title and self.author == value.author and self.price == value.price)
 
+    # Use the __ge__ method to establish >= relationship with another object
+    def __ge__(self, value):
+        if not isinstance(value, Book):
+            raise ValueError("Can't compare book to a non-book!")
+        
+        return self.price >= value.price
+
+    # Use the __lt__ method to establish < relationship with another object
+    def __lt__(self, value):
+        if not isinstance(value, Book):
+            raise ValueError("Can't compare book to a non-book!")
+        
+        return self.price < value.price
+
 b1 = Book("Python Crash Course", "Eric Matthes", 25.00)
 b2 = Book("The Scent Keeper", "Erica Bauermeister", 15.00
 )
 b3 = Book("Python Crash Course", "Eric Matthes", 25.00)
 
 # Check for equality
-print(b1 == b3) # should result in True
-print(b1 == b2) # should result in False
-print(b1 == 100) # should print ValueError message
+# print(b1 == b3) # should result in True
+# print(b1 == b2) # should result in False
+# print(b1 == 100) # should print ValueError message
 
 # Check for a greater than or lesser than value
+print(b1 >= b3) # should result in True
+print(b1 < b2) # should result in False
 
-print(b1)
-print(repr(b1))
+# Sort the books based on price, lowest to highest
+books = [b1, b3, b2]
+books.sort()
+print([book.title for book in books]) #list comprehension
