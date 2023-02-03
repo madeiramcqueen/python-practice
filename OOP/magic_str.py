19deb9686de978f60115b953226d375d2fd965f3
madeiramcqueen <madeira@mcqueen.net>
2023-02-03 12:39:11 -0800

    add eq method to compare two objects and check for equality

--- OOP/magic_str.py
+++ OOP/magic_str.py
@@ -16,7 +16,24 @@ class Book:
     def __repr__(self):
         return f"title={self.title}, author={self.author}, price={self.price}"
 
+    # Use the __eq__ method to compare and check for equality between two diff objects
+    def __eq__(self, value):
+        if not isinstance(value, Book):
+            raise ValueError("Can't compare book to a non-book!")
+
+        return (self.title == value.title and self.author == value.author and self.price == value.price)
+
 b1 = Book("Python Crash Course", "Eric Matthes", 25.00)
+b2 = Book("The Scent Keeper", "Erica Bauermeister", 15.00
+)
+b3 = Book("Python Crash Course", "Eric Matthes", 25.00)
+
+# Check for equality
+print(b1 == b3) # should result in True
+print(b1 == b2) # should result in False
+print(b1 == 100) # should print ValueError message
+
+# Check for a greater than or lesser than value
 
 print(b1)
 print(repr(b1))
