4bf3970d380190325caf7965cf695b115467c1d6
madeiramcqueen <madeira@mcqueen.net>
2023-02-12 12:25:18 -0800

    slicing practice

---
+++ Python_CrashCourse/slicing.py
@@ -0,0 +1,15 @@
+# Python Crash Course exercises Ch. 4
+
+players = ['Madeira', 'Caroline', 'Azima', 'Kayla']
+
+# Use slice method to print players Madeira-Azima
+print(players[0:3]) # index to the right of : is not included
+
+# Use slice method to print players Caroline onwards
+print(players[1:])
+
+# Use slice method to print from beginning of list to a certain index -- always starts with :
+print(players[:3])
+
+# Use slice method to print last 2 players
+print(players[-2:])
