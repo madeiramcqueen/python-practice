ec7dc9af9d10cc4c798ca9b1c6d2c220fb301121
madeiramcqueen <madeira@mcqueen.net>
2023-02-12 12:41:44 -0800

    Loop through slices

--- Python_CrashCourse/slicing.py
+++ Python_CrashCourse/slicing.py
@@ -1,6 +1,6 @@
 # Python Crash Course exercises Ch. 4
 
-players = ['Madeira', 'Caroline', 'Azima', 'Kayla']
+players = ['Madeira', 'Caroline', 'Azima', 'Kayla', 'Meredith', 'Noel']
 
 # Use slice method to print players Madeira-Azima
 print(players[0:3]) # index to the right of : is not included
@@ -13,3 +13,21 @@ print(players[:3])
 
 # Use slice method to print last 2 players
 print(players[-2:])
+
+# User slice method to skip numbers by 3
+print(players[:6:2])
+
+#Loop through a slice
+print('Here are the first three players:')
+
+for player in players[:3]:
+    print(player)
+
+# Make a copy of the list
+players_list = players[:]
+print(players_list)
+
+# Add a name to players_list then compare to make sure players_list is still a copy of player list but with added name
+players_list.append('Sean')
+print(players_list)
+print(players)
