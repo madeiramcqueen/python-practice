af53b76de5326828d7a5045972f0f552f1e4edbd
madeiramcqueen <madeira@mcqueen.net>
2023-02-16 16:10:21 -0800

    Start of dictionaries

---
+++ Python_CrashCourse/dictionaries.py
@@ -0,0 +1,6 @@
+# Python Crash Course exercises Ch. 6
+# Dictionaries = a collection of key value pairs 
+
+user = {'name': 'Madeira', 'age': '28'} # key = 'name', value = 'Madeira' --> must be in quotations
+print(user['name'])
+print(f"{user['name']} is {user['age']} years old")
