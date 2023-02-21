95c840c3f206a29ba609f142d2329b6de7412f35
madeiramcqueen <madeira@mcqueen.net>
2023-02-20 16:02:00 -0800

    iteration methods

---
+++ Advanced Python/Built-In Functions/iterators.py
@@ -0,0 +1,31 @@
+# LinkedIn Learning Course: Advanced Python by Joe Marini
+# Use iterator functions like enumerate, zip, iter, next
+
+def main():
+    # define a list of days in English and French
+    days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    daysFr = ["Dim", "Lun", "Mer", "Jeu", "Ven", "Sam"]
+
+    # use iter method to create an iterator over a collection
+    # i = iter(days)
+    # print(next(i))
+    # print(next(i))
+    # print(next(i))
+    # print(next(i))
+
+    # iterate using a function and a sentinel
+    with open("Advanced Python/Built-In Functions/testfile.txt", "r") as fp:
+        for line in iter(fp.readline, ''):
+            print(line)
+
+    # use regular interation over the days
+    for day in range(len(days)):
+        print(day+1, days[day])
+
+
+    # use enumerate to reduce code and provide a counter
+
+
+    # use zip to combine sequences
+
+print(main())
