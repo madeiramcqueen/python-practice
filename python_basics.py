4a8d77d6f6f564b9e63ebe7276bb8368e787f3f0
madeiramcqueen <madeira@mcqueen.net>
2023-03-21 11:08:49 -0700

    basic while looping

--- python_basics.py
+++ python_basics.py
@@ -53,20 +53,27 @@ t = 90
 print("Water is boiling" if t >= 100 else "Water is not boiling")
 
 # if, elif, else statements
-color = input('Enter "green", "yellow", or "red": ').lower()
-print(f'The user entered {color}')
+# color = input('Enter "green", "yellow", or "red": ').lower()
+# print(f'The user entered {color}')
 
-if color == 'green':
-    print("Go!")
-elif color == 'yellow':
-    print("Slow down!")
-elif color == 'red':
-    print("Stop!")
-else:
-    print("Bogus!")
+# if color == 'green':
+#     print("Go!")
+# elif color == 'yellow':
+#     print("Slow down!")
+# elif color == 'red':
+#     print("Stop!")
+# else:
+#     print("Bogus!")
 
 # Basic for looping
 colors = ["yellow", "red", "purple", "pink", "blue"]
 
 for color in colors:
     print(color, end=" ")
+
+# Basic while looping
+count = 0
+while count <= 10:
+    count += 1
+    print(f"The number is: {count}")
+
