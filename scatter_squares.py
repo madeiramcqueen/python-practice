f8c963c876fee0c9a26730b6484808bc04233655
madeiramcqueen <madeira@mcqueen.net>
2023-04-03 15:28:31 -0700

    Plot a series of points using scatter method

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,9 +2,18 @@
 
 import matplotlib.pyplot as plt
 
-plt.style.use("seaborn")
+# plt.style.use("seaborn")
+# fig, ax = plt.subplots()
+# ax.scatter(2, 4)
+# ax.scatter(3, 6)
+
+# Plotting a series of points using scatter()
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.style.use("seaborn-v0_8-dark")
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(x_values, y_values, s=100)
 
 # Set chart title and x and y axis labels
 ax.set_title("Scatter Plot", fontsize=24)
