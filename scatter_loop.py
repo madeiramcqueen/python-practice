7a811a7b0da6a541ae9818c45574582106008a29
madeiramcqueen <madeira@mcqueen.net>
2023-04-03 15:37:00 -0700

    Use a for loop to display lots of data

---
+++ scatter_loop.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+# Use a loop to configure and display data calculations
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values] # calculate the square root of each x value
+
+plt.style.use("seaborn-v0_8-dark")
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=10)
+
+# Set chart title and label axis
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set range for each axis
+ax.axis([0, 1100, 0, 1100000])
+
+plt.show()
