38d7a71aa9675b08f084af8b66d5a83ddd7505c1
madeiramcqueen <madeira@mcqueen.net>
2023-04-15 13:22:16 -0700

    Add color to bar chart

--- mpl_barGraph.py
+++ mpl_barGraph.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 country = ["A", "B", "C", "D", "E"]
 gdp_per_capita = [45000, 42000, 52000, 49000, 47000]
 
+
+# customize color per country
+colors = ["Blue", "Green", "Purple", "Pink", "Yellow", "Brown"]
+
 # add labels and type of graph
-plt.bar(country, gdp_per_capita)
+plt.bar(country, gdp_per_capita, color=colors)
 plt.title("Country & GDP Per Capita")
 plt.xlabel("Country")
 plt.ylabel("GDP Per Capita")
